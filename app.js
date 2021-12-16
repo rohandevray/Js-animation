14efeb538fe263352559e8b10e8604ed1e2389be
Rohan Dev Ray <91519880+rohandevray@users.noreply.github.com>
2021-12-17 02:47:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,30 +24,30 @@ if(destination.index ===1){
 //text animation for 3rd section
 
 
-const text =document.querySelector(".fancy");//be careful while selecting the classes
-const strText =text.textContent;
-const splitText =strText.split("");//splits every word including spaces into an array and then converting it into span so we can indivisully animate all tha letters one by one
-text.textContent="";//as it get doubly printed so to remove it ..
-for(let i=0;i < splitText.length ;i++){
+// const text =document.querySelector(".fancy");//be careful while selecting the classes
+// const strText =text.textContent;
+// const splitText =strText.split("");//splits every word including spaces into an array and then converting it into span so we can indivisully animate all tha letters one by one
+// text.textContent="";//as it get doubly printed so to remove it ..
+// for(let i=0;i < splitText.length ;i++){
 
-  text.innerHTML += "<span>" +splitText[i] +"</span>";
-}
-//text.textContent and text.innerHTML is the same thing
+//   text.innerHTML += "<span>" +splitText[i] +"</span>";
+// }
+// //text.textContent and text.innerHTML is the same thing
 
-let char=0;
-let timer =setInterval(onTick,50);
+// let char=0;
+// let timer =setInterval(onTick,50);
 
-function onTick(){
-   const span =document.querySelectorAll("span")[char];//span is html inline html 
-   span.classList.add("fade");//can add any class name to apply
-   char++;
-   if(char === splitText.length){
-     complete();
-     return;
-   }
-}
+// function onTick(){
+//    const span =document.querySelectorAll("span")[char];//span is html inline html 
+//    span.classList.add("fade");//can add any class name to apply
+//    char++;
+//    if(char === splitText.length){
+//      complete();
+//      return;
+//    }
+// }
 
-function complete(){
-  clearInterval(timer);//stops the timer
-  timer=null;//starts from beginning
-}
+// function complete(){
+//   clearInterval(timer);//stops the timer
+//   timer=null;//starts from beginning
+// }
